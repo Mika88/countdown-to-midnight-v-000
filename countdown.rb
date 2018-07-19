8e1b8d54985a8b2a3ea62d52bd2bffdf14bad6b5
Mika88 <mikaspivak@gmail.com>
2018-07-19 02:42:49 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 def countdown(number)
   counter = number
   while counter > 0
-    puts "#{counter} SECOND(S)!"
+    puts "\n#{counter} SECOND(S)!"
     counter -= 1
     end
     "HAPPY NEW YEAR!"
